d2467a21538a2035ec0d59754674a46bc9a5dcc3
Luis Soares <luis.xsoares@gmail.com>
2014-01-07 17:29:49 +0000

    Changed structure

---
+++ vhostmanager/src/com/gemabit/vhostmanager/ui/VirtualHostManager.java
@@ -0,0 +1,23 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.gemabit.vhostmanager.ui;
+
+/**
+ *
+ * @author lsoares
+ */
+public class VirtualHostManager {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        Main window = new Main();
+        window.setResizable(false);
+        window.setVisible(true);
+        
+    }
+}
